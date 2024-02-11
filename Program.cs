5ef8321ec9597cfabe7be1fab45cb4a484ace6de
_skosik_ <artemon271@ya.ru>
2024-02-11 12:33:36 +0500

    third task

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿/* 
-Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
-Напишите программу, которая покажет количество чётных чисел в массиве.
+Задача 3: Напишите программу, которая перевернёт одномерный массив 
+(первый элемент станет последним, второй – предпоследним и т.д.)
 */
 
 Console.Clear();
@@ -10,18 +10,19 @@ int[] array = new int[n];
 
 int[] inputArray(int[] array){
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(100, 1000); // [100, 999]
+        array[i] = new Random().Next(1, 301); // [100, 999]
     return array;
 }
 
-void countEvenNumbers (int[] array) {
-    int count = 0;
+void expandArr (int[] array) {
     Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
-    foreach (int element in array){
-        if (element % 2 == 0)
-            count++;
+    int tmp;
+    for (int i = 0; i < array.Length / 2; i++){
+        tmp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = tmp;
     }
-    Console.WriteLine(count);
+    Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
 }
 
-countEvenNumbers(inputArray(array));
+expandArr(inputArray(array));
